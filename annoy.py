87d7072b612882ad38eee38e7446f9b189eca62c
Kinjal Haldar <kinjal.haldar@gmail.com>
2023-09-07 16:43:59 -0400

    rewrote parallel annoy

--- annoy.py
+++ annoy.py
@@ -1,42 +1,8 @@
 from __future__ import annotations
 import numpy as np
 import random
-
-
-class AnnoyIndex(object):
-
-    def __init__(self, x: np.ndarray, k: int):
-        self._root = self._build_tree(x, k)
-
-    def _build_tree(self, x: np.ndarray, k: int) -> _AnnoyNode:
-        n = np.shape(x)[0]
-        if n <= k:
-            return _AnnoyNode(x, is_leaf=True)
-
-        i, j = random.sample(range(0, n), 2)
-        node = _AnnoyNode(np.stack([x[i], x[j]]))
-
-        d_i = np.linalg.norm(x - x[i], axis=1)
-        d_j = np.linalg.norm(x - x[j], axis=1)
-        left = x[np.where(d_i <= d_j)]
-        right = x[np.where(d_i > d_j)]
-        node.left = self._build_tree(left, k)
-        node._right = self._build_tree(right, k)
-
-        return node
-
-    def query(self, q: np.ndarray):
-        node = self._root
-        while not node.is_leaf:
-            d_left = np.linalg.norm(q - node.vects[0])
-            d_right = np.linalg.norm(q - node.vects[1])
-            node = node.left if d_left < d_right else node._right
-
-        best_match = node.vects[0]
-        for v in node.vects:
-            if np.linalg.norm(q - v) < np.linalg.norm(q - best_match):
-                best_match = v
-        return best_match
+import multiprocessing as mp
+import utils
 
 
 class _AnnoyNode(object):
@@ -45,7 +11,7 @@ class _AnnoyNode(object):
         vects: np.ndarray,
         left: _AnnoyNode = None,
         right: _AnnoyNode = None,
-        is_leaf: bool = False
+        is_leaf: bool = False,
     ):
         self._vects = vects
         self._left = left
@@ -69,12 +35,12 @@ class _AnnoyNode(object):
         self._left = left
 
     @property
-    def left(self) -> _AnnoyNode:
-        return self._left
+    def right(self) -> _AnnoyNode:
+        return self._right
 
-    @left.setter
-    def left(self, left: _AnnoyNode) -> None:
-        self._left = left
+    @right.setter
+    def right(self, right: _AnnoyNode) -> None:
+        self._right = right
 
     @property
     def is_leaf(self) -> bool:
@@ -85,9 +51,110 @@ class _AnnoyNode(object):
         self._is_leaf = is_leaf
 
 
+class _AnnoyTree(object):
+    def __init__(self, x: np.ndarray, k: int):
+        self._size = np.shape(x)[0]
+        if k > self._size:
+            raise ValueError("k exceeds index size: k={}, size={}".format(k, self.size))
+        self._root = self._build_tree(x, k)
+
+    @property
+    def size(self, size: int) -> int:
+        return self._size
+
+    def _build_tree(self, x: np.ndarray, k: int) -> _AnnoyNode:
+        n = np.shape(x)[0]
+        if n <= k:
+            return _AnnoyNode(x, is_leaf=True)
+
+        i, j = random.sample(range(0, n), 2)
+        node = _AnnoyNode(np.stack([x[i], x[j]]))
+
+        d_i = np.linalg.norm(x - x[i], axis=1)
+        d_j = np.linalg.norm(x - x[j], axis=1)
+        left = x[np.where(d_i < d_j)]
+        right = x[np.where(d_i >= d_j)]
+        node.left = self._build_tree(left, k)
+        node._right = self._build_tree(right, k)
+
+        return node
+
+    def query(self, q: np.ndarray, n: int) -> np.ndarray:
+        res = self._query_rec(self._root, q, n)
+        return utils.sort_dist_to_v(q, res)
+
+    def _query_rec(self, node: _AnnoyNode, q: np.ndarray, n: int) -> np.ndarray:
+        if node.is_leaf:
+            return node.vects[:min(np.shape(node.vects)[0], n)]
+
+        d_left = np.linalg.norm(q - node.vects[0])
+        d_right = np.linalg.norm(q - node.vects[1])
+        left = d_left < d_right
+        res = self._query_rec(node.left if left else node.right, q, n)
+        size = np.shape(res)[0]
+        if size < n:
+            res = np.vstack((res, self._query_rec(node.right if left else node.left, q, n)))
+        return res[:min(np.shape(res)[0], n)]
+
+
+class AnnoyIndex(object):
+    def __init__(self):
+        self._size = 0
+        self._k = 0
+        self._n = 0
+        self._q = np.zeros((0, 0))
+        self._trees = []
+
+    @property
+    def size(self) -> int:
+        return self._size
+
+    def build(
+        self,
+        x: np.ndarray,
+        n_trees: int,
+        k: int,
+        parallelize: bool = True,
+        shuffle: bool = False
+    ) -> None:
+        self._k = k
+
+        if shuffle:
+            x = np.copy(x)
+            np.random.shuffle(x)
+        x_splits = np.array_split(x, n_trees)
+        if parallelize:
+            with mp.Pool(n_trees) as p:
+                self._trees = p.map(self._build_tree, x_splits)
+        else:
+            self._trees = list(map(self._build_tree, x_splits))
+        self._size = np.shape(x)[0]
+
+    def query(self, q: np.ndarray, n: int, parallelize: bool = False) -> np.ndarray:
+        self._q = q
+        self._n = n
+
+        res_pool = []
+        if parallelize:
+            with mp.Pool(len(self._trees)) as p:
+                res_pool = p.map(self._query_tree, self._trees)
+        else:
+            res_pool = list(map(self._query_tree, self._trees))
+        res = utils.sort_dist_to_v(q, np.vstack(res_pool))
+        return res[:min(np.shape(res)[0], n)]
+
+    def _build_tree(self, x: np.ndarray) -> _AnnoyTree:
+        return _AnnoyTree(x, self._k)
+
+    def _query_tree(self, t: _AnnoyTree) -> np.ndarray:
+        return t.query(self._q, self._n)
+
+
 if __name__ == "__main__":
     x = np.ndarray(shape=(1000, 3))
     for i in range(0, np.shape(x)[0]):
         x[i] = np.array([i, i, i])
-    index = AnnoyIndex(x, 5)
-    print(index.query(np.array([11, 12, 10.5])))
+
+    index = AnnoyIndex()
+    index.build(x=x, n_trees=5, k=10, parallelize=True, shuffle=True)
+    print(index.query(np.array([31, 31, 31]), n=20, parallelize=False))
---
+++ perf_test.py
@@ -0,0 +1,104 @@
+import multiprocessing as mp
+from tqdm import tqdm
+import numpy as np
+import time
+import annoy
+import utils
+import matplotlib.pyplot as plt
+
+
+def linear_search(x: np.ndarray, q: np.ndarray, n: int):
+    return utils.sort_dist_to_v(q, x)[:n]
+
+
+if __name__ == "__main__":
+    iters = 100  # num iters to average over
+    m = 16   # dim of each vector
+    nn = 20  # number of nearest neighbors
+    n_cores = mp.cpu_count()  # number of CPU cores (max num. trees)
+
+    n_min = 100
+    n_max = 1000
+    n_step = 100
+
+    perf_exhaustive = []
+    perf_build_st = []
+    perf_build_parallel = []
+    perf_query_st = []
+    perf_query_parallel = []
+    for i in range(1, mp.cpu_count() + 1):
+        perf_build_st.append([])
+        perf_build_parallel.append([])
+        perf_query_st.append([])
+        perf_query_parallel.append([])
+
+    for n in range(n_min, n_max + 1, n_step):
+        print("iters = {}, size = {} x {}, nn = {}\n".format(iters, n, m, nn))
+
+        x = np.random.rand(n, m)
+
+        t_exh = 0
+        for i in tqdm(range(iters)):
+            q = np.random.rand(m)
+            t_0 = time.perf_counter()
+            linear_search(x, q, nn)
+            t_1 = time.perf_counter()
+            t_exh += t_1 - t_0
+        t_exh /= iters
+        perf_exhaustive.append(t_exh)
+        print("exhaustive search: {}".format(t_exh))
+
+        k = 5
+
+        for trees in range(1, mp.cpu_count() + 1):
+            t_build_single_thread = 0
+            t_build_parallel = 0
+            t_query_single_thread = 0
+            t_query_parallel = 0
+            for i in tqdm(range(iters)):
+                q = np.random.rand(m)
+                index = annoy.AnnoyIndex()
+                t_0 = time.perf_counter()
+                index.build(x=x, n_trees=trees, k=k, parallelize=False, shuffle=False)
+                t_1 = time.perf_counter()
+                index.build(x=x, n_trees=trees, k=k, parallelize=True, shuffle=False)
+                t_2 = time.perf_counter()
+                index.query(q=q, n=nn, parallelize=False)
+                t_3 = time.perf_counter()
+                index.query(q=q, n=nn, parallelize=True)
+                t_4 = time.perf_counter()
+
+                t_build_single_thread += t_1 - t_0
+                t_build_parallel += t_2 - t_1
+                t_query_single_thread += t_3 - t_2
+                t_query_parallel += t_4 - t_3
+
+            t_build_single_thread /= iters
+            t_build_parallel /= iters
+            t_query_single_thread /= iters
+            t_query_parallel /= iters
+
+            perf_build_st[trees - 1].append(t_build_single_thread)
+            perf_build_parallel[trees - 1].append(t_build_parallel)
+            perf_query_st[trees - 1].append(t_query_single_thread)
+            perf_query_parallel[trees - 1].append(t_query_parallel)
+
+            print(
+                "MP: trees: {}, build(s.t.): {}, build(parallel): {}, query (s.t.): {}, query(parallel): {}".format(
+                    trees,
+                    t_build_single_thread,
+                    t_build_parallel,
+                    t_query_single_thread,
+                    t_query_parallel,
+                )
+            )
+
+    x_plt = np.arange(start=n_min, stop=n_max + 1, step=n_step)
+    plt.plot(x_plt, perf_exhaustive, label="exhaustive search")
+    for i in range(1, mp.cpu_count() + 1):
+        plt.plot(x_plt, perf_build_st[i - 1], label="build-single-thread-{}-trees".format(i))
+        plt.plot(x_plt, perf_build_parallel[i - 1], label="build-parallel-{}-trees".format(i))
+        plt.plot(x_plt, perf_query_st[i - 1], label="query-single-thread-{}-trees".format(i))
+        plt.plot(x_plt, perf_query_parallel[i - 1], label="query-parallel-{}-trees".format(i))
+    plt.legend()
+    plt.show()
